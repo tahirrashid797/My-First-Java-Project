e6f382f4db1b9b2d598ee5703cf727bfe31d5b95
tahirrashid797 <tahirrashid894@gmail.com>
2026-05-01 19:53:52 +0500

    Refactor password analyzer with try-with-resources

---
+++ README.java
@@ -0,0 +1,37 @@
+ import java.util.Scanner;
+
+public class project {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("===  Cyber Security Password Analyzer ===");
+            System.out.println("Enter password to check: ");
+            String password = input.nextLine();
+            
+            int score = 0;
+            
+            // 1. Length Check
+            if (password.length() >= 8) score++;
+            if (password.length() >= 12) score++;
+            
+            if (password.matches(".*[0-9].*")) score++;
+            
+            if (password.matches(".*[A-Z].*")) score++;
+            
+            if (password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) score++;
+            
+            System.out.println("\nAnalyzing...");
+            System.out.print("Strength: ");
+            
+            if (score <= 2) {
+                System.out.println(" WEAK (Hackable in seconds!)");
+            } else if (score <= 4) {
+                System.out.println("MEDIUM (Good, but could be better)");
+            } else {
+                System.out.println("STRONG (Secure like a vault!)");
+            }
+        }
+    }
+}
+//import java.util.Scanner;
+
+    
--- README.md
+++
@@ -1,1 +0,0 @@
-# My-First-Java-Project
